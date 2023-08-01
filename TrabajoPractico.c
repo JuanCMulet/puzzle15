ee33a943c4a427f44a7dc600206add397ea028ce
sofimdi <sofimdi4z@gmail.com>
2023-08-01 19:44:10 -0300

    add rounds and timed game modes

--- TrabajoPractico.c
+++ TrabajoPractico.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#ifdef __unix__ // Unix/Linux-specific header file
+    #include <unistd.h> 
+    #include <curses.h>
+    #include <sys/time.h>
+#elif defined(__WIN32__) || defined(__WIN64__) // Windows-specific header file
+    #include <windows.h> 
+#endif
+
 #define SIZE 16
+#define EMPTY -1
+
+
+
+double getTimeUnix(struct timeval start, struct timeval end) {
+    //Function in Unix to calculate the elapsed while inside the program.
+    return (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec) / 1000000.0;
+}
+
+double getTimeWindows(SYSTEMTIME start, SYSTEMTIME end) {
+    //Function in Windows to calculate the elapsed while inside the program.
+    FILETIME ftStart, ftEnd;
+    ULARGE_INTEGER uliStart, uliEnd;
+
+    SystemTimeToFileTime(&start, &ftStart);
+    SystemTimeToFileTime(&end, &ftEnd);
+
+    uliStart.LowPart = ftStart.dwLowDateTime;
+    uliStart.HighPart = ftStart.dwHighDateTime;
+    uliEnd.LowPart = ftEnd.dwLowDateTime;
+    uliEnd.HighPart = ftEnd.dwHighDateTime;
+
+    return (double)(uliEnd.QuadPart - uliStart.QuadPart) / 10000000.0;
+}
 
 int emptySquareRow(int index) {
     int row;
@@ -122,6 +154,7 @@ int moveSpace(int* grid, int size) {
         switch (option) { // Logic for keyboard movement.
             case 'W':
             case 'w':
+                //Safetey guards when trying to move outside the grid.
                 if (loc == 0 || loc == 1 || loc == 2 || loc == 3) {
                     printf("\nInvalid move\n");
                     check = 0;
@@ -181,10 +214,10 @@ int moveSpace(int* grid, int size) {
 int checkWin(const int grid[], const int gridWin[], int size) {
     for (int i = 0; i < size; i++) {
         if (grid[i] != gridWin[i]) {
-            return 0; // Arrays are not equal
+            return 0; //Arrays are not equal.
         }
     }
-    return 1; // Arrays are equal
+    return 1; //Arrays are equal.
 }
 
 int betMode() {
@@ -207,17 +240,19 @@ int betMode() {
 
 int calcPoints(int moves, int bet) {
     if (moves < bet && moves >= bet-10) {
-        return 500;
+        return 500; //If done between M and M-10 moves.
     } else if (moves > bet && moves <= bet+10) {
-        return 100;
+        return 100; //If done between M and M+10 moves.
     } else if (moves == bet) {
-        return 1000;
+        return 1000; //If done in exactly M moves.
     } else {
         return 0;
     }
 }
 
-int gamePlay(int* grid, int* winGrid, int size, int* moves) {
+int gamePlay(int* grid, int* winGrid, int size, int bet) {
+    int totalMoves = 0;
+    int score = 0;
     int ending; // 0 Normal state - 1 ended by quiting - 2 ended by wining
     
     do { // We ask the player to move the square...
@@ -227,50 +262,134 @@ int gamePlay(int* grid, int* winGrid, int size, int* moves) {
             printf("No points for quitters!\n");
         } else if (checkWin(grid, winGrid, size) == 1) {
             ending = 1;
-            *moves += 1;
+            totalMoves += 1;
             printf("\nYou won!\n");
         } else {
-            *moves += 1; // We count the total moves made in the game;
+            totalMoves += 1; // We count the total moves made in the game;
             ending = 0;
         }
     } while (ending < 1); //... until the game its solved or exit.
-
-    return ending;
-}
-
-void createGame(int* grid, int* winGrid, int size, int gameMode) {
-    int totalMoves = 0;
-    int movesBet = 0;
-    int score = 0;
-    int gameResult;
     
-    /*do { // We create random grid starts...
-        randomizeGrid(grid, size);
-    } while (viableGrid(grid,size) != 1); //...until its a playable one.*/
-    
-    if (gameMode == 1) {
-        gamePlay(grid, winGrid, size, &totalMoves);
-    } else if (gameMode == 2) {
-        movesBet = betMode();
-        gameResult = gamePlay(grid, winGrid, size, &totalMoves);
-        if (gameResult == 2) {
-            score = calcPoints(totalMoves, movesBet);
+    if (bet > 0) {
+        //If there was a bet for moves we run this to calculate points and print the results.
+        if (ending == 1) {
+            score = calcPoints(totalMoves, bet);
             printf("Total moves: %d\n", totalMoves);
-            printf("Moves bet: %d\n", movesBet);
-            printf("\nYou Total Score is: %d", score);
-        } else {
+            printf("Moves bet: %d\n", bet);
+            printf("\nYou Total Score is: %d\n\n", score);
+        } else if (ending == 2) {
+            score = -1;
             printf("Next time for sure!\n");
         }
     }
     
+    return score;
+}
+
+void gameMode(int* grid, int* winGrid, int size, int op1, int op2, int op3) {
+    // op1 - Bet / op2 - 10rounds / op3 - timer
+    int movesBet = 0;
     
+    if (op2 == 1) {
+        // We initialize an array to store all the games scores.
+        int allScores[10][2] = {
+            {EMPTY, EMPTY}, 
+            {EMPTY, EMPTY}, 
+            {EMPTY, EMPTY}, 
+            {EMPTY, EMPTY}, 
+            {EMPTY, EMPTY}, 
+            {EMPTY, EMPTY}, 
+            {EMPTY, EMPTY}, 
+            {EMPTY, EMPTY}, 
+            {EMPTY, EMPTY}, 
+            {EMPTY, EMPTY}
+            };
+        int totalGames = 1;
+        int gameResult;
+        
+        //We use this to store the timer values depending on operative system
+        #ifdef __unix__
+            struct timeval start, end;
+        #elif defined(__WIN32__) || defined(__WIN64__)
+            SYSTEMTIME start, end;
+        #endif
+        double totalTime;
+        int minutes, seconds;
+        
+        do {
+            movesBet = betMode(); //Store the moves bet for the current game.
+            if (op3 == 1) {
+                //We initialize the timer for the current game.
+                #ifdef __unix__
+                    gettimeofday(&start, NULL);
+                #elif defined(__WIN32__) || defined(__WIN64__)
+                    GetSystemTime(&start);
+                #endif
+            }
+            gameResult = gamePlay(grid, winGrid, size, movesBet); //Game starts.
+            if (op3 == 1) {
+                //Once the game is won we stop the timer.
+                #ifdef __unix__
+                    gettimeofday(&end, NULL);
+                    totalTime = getTimeUnix(start, end);
+                #elif defined(__WIN32__) || defined(__WIN64__)
+                    GetSystemTime(&end);
+                    totalTime = getTimeWindows(start, end);
+                #endif
+            }
+            allScores[totalGames-1][0] = gameResult; //We store the score...
+            allScores[totalGames-1][1] = totalTime; //And tme for that game.
+            totalGames++; //We add 1 each time to check once 10 games have completed.
+            if (gameResult > -1) {
+                //We run this after a game is finished independent of winning or quitting.
+                for (int i = 0; i < 10; i++) {
+                    //Then we print the scores only if the row in the array has a value.
+                    if (allScores[i][0] != EMPTY) {
+                        if (allScores[i][1] != EMPTY) {
+                            minutes = (int)totalTime / 60;
+                            seconds = (int)totalTime % 60;
+                            printf("Ronda %d: %d Puntos en %d:%02d minutos\n", i+1, allScores[i][0], minutes, seconds);
+                            
+                        } else {
+                            printf("Ronda %d: %d Puntos\n", i+1, allScores[i][0]);
+                        }
+                    }
+                }
+            }
+        } while (gameResult > -1 && totalGames <= 10); //We run this entire code until either 10 games have been played or the player quits.
+    } else {
+        if (op1 == 1) {
+            //If Normal Bet mode was selected.
+            movesBet = betMode();
+            gamePlay(grid, winGrid, size, movesBet);
+        } else {
+            //If Normal mode was selected.
+            gamePlay(grid, winGrid, size, movesBet);
+        }
+    }
+}
+
+void createGame(int* grid, int* winGrid, int size, int mode) {
+    /*do { // We create random grid starts...
+        randomizeGrid(grid, size);
+    } while (viableGrid(grid,size) != 1); //...until its a playable one.*/
+    
+    if (mode == 4) {
+        gameMode(grid, winGrid, size, 1, 1, 1);
+    } else if (mode == 3) {
+        gameMode(grid, winGrid, size, 1, 1, 0);
+    } else if (mode == 2) {
+        gameMode(grid, winGrid, size, 1, 0, 0);
+    } else {
+        gameMode(grid, winGrid, size, 0, 0, 0);
+    }
 }
 
 int main() {
     int gameGrid[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 15};
     int gameWin[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     
-    createGame(gameGrid, gameWin, SIZE, 2);
+    createGame(gameGrid, gameWin, SIZE, 4);
     
 
     return 0;
